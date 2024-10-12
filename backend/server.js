29d5db074d2fe636d9933cafdb67f3ed456327e7
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2024-10-12 12:51:40 -0700

    Connect Express.js to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
+const mongoose = require('mongoose');
+const MONGO_URI = 'mongodb+srv://angelahao299:Mongoclouds6%40@nutrition-cluster.smq9k.mongodb.net/?retryWrites=true&w=majority&appName=nutrition-cluster';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
